a50521d67feefdcca358e4c444d4384ab618446a
Josh Doyle <me@joshdoyle.us>
2020-03-24 15:59:34 -0500

    completed #54 with all tests passed

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -18,7 +18,7 @@ class UserSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         """Update a user, setting the password correctly and return it"""
         password = validated_data.pop('password', None)
-        user = super().update(instance,validated_data)
+        user = super().update(instance, validated_data)
 
         if password:
             user.set_password(password)
